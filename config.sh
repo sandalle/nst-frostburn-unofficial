b741f6a71368b529416660bc275476567c559d30
Eric Sandall <sandalle@gmail.com>
2016-09-22 15:27:52 -0700

    Default 1.10 mod config files plus start script

---
+++ config.sh
@@ -0,0 +1,7 @@
+#just to set some globals used by the other scripts
+FORGE='forge-1.10.2-12.18.1.2093-universal.jar'
+SERVER_DIR='/home/minecraft/multicraft/servers/mc1.10'
+START_MEMORY=5120
+MAX_MEMORY=5120
+
+[[ -f /etc/profile.d/java.sh ]] && . /etc/profile.d/java.sh
---
+++ start.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Settings
+[[ -f config.sh ]] && . config.sh
+SERVICE="${FORGE:-server.jar}"
+OPTIONS='nogui'
+MCPATH="${SERVER_DIR}"
+#	-Dfml.debugClassPatchManager=true -Dfml.debugRegistryEntries=true \
+#	-XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:ParallelGCThreads=8 -XX:ConcGCThreads=2 -XX:InitiatingHeapOccupancyPercent=70 -XX:+DisableExplicitGC -XX:+UseFastAccessorMethods -XX:+AggressiveOpts -XX:-UseAdaptiveSizePolicy \
+INVOCATION="java -server  \
+	-Xms${START_MEMORY}M -Xmx${MAX_MEMORY}M  \
+	-XX:+UseLargePages  \
+	-XX:+UseLargePagesInMetaspace  \
+	-XX:+UseG1GC  \
+	-XX:+UnlockExperimentalVMOptions  \
+	-XX:MaxGCPauseMillis=100  \
+	-XX:+DisableExplicitGC  \
+	-XX:TargetSurvivorRatio=90  \
+	-XX:G1NewSizePercent=50  \
+	-XX:G1MaxNewSizePercent=80 \
+	-XX:InitiatingHeapOccupancyPercent=10  \
+	-XX:G1MixedGCLiveThresholdPercent=50  \
+	-XX:+AggressiveOpts  \
+	-XX:+AlwaysPreTouch  \
+	-Djline.terminal=jline.UnsupportedTerminal -Djava.util.prefs.userRoot=${SERVER_DIR}  \
+	-Dfml.doNotBackup=true -Dfml.queryResult=confirm  \
+	-Djava.net.preferIPv4Stack=true  \
+	-jar $SERVICE $OPTIONS"
+
+${INVOCATION}
