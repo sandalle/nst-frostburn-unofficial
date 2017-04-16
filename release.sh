62eeb077761c50d13c6ed3449a4e432ac6d1b36d
Eric Sandall <sandalle@gmail.com>
2017-04-16 00:26:12 -0700

    Better release versioning and push

--- release.sh
+++ release.sh
@@ -3,6 +3,15 @@
 # Make a new release based on VERSION
 GPG_USER='sandalle@gmail.com'
 VERSION=$(cat VERSION)
+NEXT_VERSION=$(perl -pe 's/^((\d+\.)*)(\d+)(.*)$/$1.($3+1).$4/e' <<< ${VERSION})
+
+# Tag current version
 git tag -u ${GPG_USER} --sign -m "frostburn-${VERSION}" "frostburn-${VERSION}"  &&
+
+# Increment to next version
+echo ${NEXT_VERSION} > VERSION  &&
+git commit -m "Starting ${NEXT_VERSION}" VERSION  &&
+
+# Push new version and tags
 git push --all  &&
 git push --tags
