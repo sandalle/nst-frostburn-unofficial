6d3cf9355e3e17c268023a77d609f59889ebfccd
Eric Sandall <sandalle@gmail.com>
2017-04-28 18:50:40 -0700

    Revert "Go Java, server runs even better with 3G than with 4G allocated..."
    This reverts commit 58e3355c8e68e96c83e1eedded500eac7e72a07b.
    
    Reverting back to 5G

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
 #just to set some globals used by the other scripts
 FORGE='forge-1.10.2-12.18.3.2281-universal.jar'
 SERVER_DIR='/home/minecraft/multicraft/servers/mc1.10'
-START_MEMORY=3072
-MAX_MEMORY=3072
+START_MEMORY=4096
+MAX_MEMORY=4096
 
 [[ -f /etc/profile.d/java.sh ]] && . /etc/profile.d/java.sh
