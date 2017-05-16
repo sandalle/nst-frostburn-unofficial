63a113540fd8ecb2eba54d4a955b1ea938106a01
Eric Sandall <sandalle@gmail.com>
2017-05-15 20:51:53 -0700

    Forge Mod Loader 1.10.2-12.18.3.2297
    Updated mods:
      Advanced Rocketry 1.10.2-1.0.19
      Chameleon 1.10.2-2.3.0
      LibVulpes 1.10.2-0.1.19
      Mystical Agriculture 1.5.3
      Natura 1.10.2-4.1.0.57
      Storage Drawers 1.10.2-3.7.6
      The One Probe 1.1x-1.4.7

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 #just to set some globals used by the other scripts
-FORGE='forge-1.10.2-12.18.3.2281-universal.jar'
+FORGE='forge-1.10.2-12.18.3.2297-universal.jar'
 SERVER_DIR='/home/minecraft/multicraft/servers/mc1.10'
 START_MEMORY=5120
 MAX_MEMORY=5120
