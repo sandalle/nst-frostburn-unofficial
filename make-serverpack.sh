eca7c50b2a196c7b81fdfefbf502185c91fbd2e6
Eric Sandall <sandalle@gmail.com>
2017-07-24 18:37:31 -0700

    Add script to make a simple server pack

---
+++ make-serverpack.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# $1 = server directory to archive
+
+if [[ "x$#" != "x1" ]]; then
+	echo "Usage: ${0} <server directory>"  &&
+	exit 1
+fi &&
+
+# Include these files/folders
+archiveList="asm/ \
+	config.sh \
+	config/ \
+	forge*.jar \
+	libraries/ \
+	modpack/ \
+	mods/ \
+	scripts/ \
+	start.sh"
+
+# Exclude these files/folders
+excludeList="config/purpleirc/bots/*"
+
+cd "${1}" &&
+zip -r ../"${1///}".zip ${archiveList} --exclude ${excludeList} &&
+cd -
