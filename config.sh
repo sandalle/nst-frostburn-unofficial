408fbd40ee9dfb3b4c91b14b3da5695c521970f6
Eric Sandall <sandalle@gmail.com>
2018-01-27 00:13:08 -0800

    config.sh: Use present working directory for SERVER_DIR

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #just to set some globals used by the other scripts
 FORGE='forge-1.10.2-12.18.3.2511-universal.jar'
-SERVER_DIR='/home/minecraft/multicraft/servers/mc1.10'
+SERVER_DIR="$(pwd)"
 START_MEMORY=5120
 MAX_MEMORY=5120
 
